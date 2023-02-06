1d736fdfbe0ef2b378ad963643d15cc9ae979640
Amandeep Kalakar <amankumaradk@gmail.com>
2023-02-06 19:21:51 +0530

    commit

---
+++ React JS/outputting_lists/src/Home.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome Party", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs!" />
+      {/* {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))} */}
+
+      {/* Reusing Components */}
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's blogs"
+      />
+    </div>
+  );
+};
+
+export default Home;
