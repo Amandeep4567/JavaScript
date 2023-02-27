3017e15a089582be18b18f15afc5bfb517858312
Amandeep Kalakar <amankumaradk@gmail.com>
2023-02-28 00:40:53 +0530

    updated

--- React JS/conditional_loading_message/src/Home.js
+++ React JS/conditional_loading_message/src/Home.js
@@ -1,33 +1,13 @@
 import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          // console.log(res);
-          if (!res.ok) {
-            throw Error("could not fetch the data for that resourse");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
