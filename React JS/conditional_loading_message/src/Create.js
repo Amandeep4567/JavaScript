d90005624dd70f220e993858f25e883d88761a6c
Amandeep Kalakar <amankumaradk@gmail.com>
2023-03-09 18:48:53 +0530

    code updated

--- React JS/conditional_loading_message/src/Create.js
+++ React JS/conditional_loading_message/src/Create.js
@@ -4,11 +4,30 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const blog = { title, body, author };
+    // console.log(blog);
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
+  };
 
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -27,11 +46,12 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button>Adding blog...</button>}
       </form>
-      <p>{title}</p>
+      {/* <p>{title}</p>
       <p>{body}</p>
-      <p>{author}</p>
+      <p>{author}</p> */}
     </div>
   );
 };
